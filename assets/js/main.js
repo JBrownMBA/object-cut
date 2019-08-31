0b37605a567d94c7f9eb0bc4135de23636fa078c
Albert Suarez <alsumo95@gmail.com>
2019-08-31 14:41:07 +0200

    Added more input into the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-$('.dropdown').dropdown();
+// Dropdown
+$('.form .dropdown').dropdown();
+
+// Checkbox
+$('#input-file').hide();
+$('.form .checkbox').checkbox();
+$('.form .checkbox').checkbox({
+    onChecked: function () {
+        if ($('#from-url').is(':checked')) {
+            $('#input-file').hide();
+            $('#input-url').show();
+        } else {
+            $('#input-url').hide();
+            $('#input-file').show();
+        }
+    }
+});
