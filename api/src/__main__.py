bbf4437dfd3f2fd7b9bc3ef66c379cef0bfd1a17
Albert Suarez <alsumo95@gmail.com>
2019-08-31 20:11:38 +0200

    Removed config from UWSGI and added pyOpenSSL

--- api/src/__main__.py
+++ api/src/__main__.py
@@ -2,4 +2,4 @@ from src.object_cut import connexion_app
 
 
 if __name__ == '__main__':
-    connexion_app.run(host='0.0.0.0', port=8083)
+    connexion_app.run(host='0.0.0.0', port=8083, ssl_context='adhoc')
